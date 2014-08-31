40cd787e49c009ccf03b72eea12d1f9afd500f8e
shota <riodeja.5.shota@gmail.com>
2014-08-31 22:14:50 +0900

    Improve the README file

--- rails_projects/demo_app/db/schema.rb
+++ rails_projects/demo_app/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828142520) do
+ActiveRecord::Schema.define(version: 20140830073942) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
